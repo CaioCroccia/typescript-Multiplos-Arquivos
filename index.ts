edda615acb8bbd472121bb60378f13117346d9ad
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-08-28 21:40:47 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 // Import stylesheets
 import './style.css';
 
+
+let greeter = new Greeter('Caioba');
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `
+<h1>Trabalhando com multiplos arquivos</h1>
+<h2>${greeter.GetGreet()}</h2>`;
